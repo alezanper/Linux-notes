6861ae3e8fec28c1d363a72ec5f504232ca77031
fa.benavideso@uniandes.edu.co <fabenavideso@gmail.com>
2021-06-13 11:44:54 -0500

    [added] basic commands

---
+++ Basic_commands.sh
@@ -0,0 +1,32 @@
+#BASIC LINUX COMMANDS
+# They are case sensitive
+
+# ls is for checking directories	
+ls
+ls -l		# Long listing format
+ls /bin		# Check all available commands
+ls --help   # Check help documentation
+
+# Change directory
+cd
+
+# Check current directory	
+pwd
+
+# concatenates and display files
+cat
+
+# see variable
+echo
+echo $PATH  # Read PATH variable
+
+# Displays the online manual
+man		
+man ls  # Check manual for ls
+man -k calendar		# check page content of documentation for calendar
+
+# Locates a command location
+which		
+which cat	
+
+	
